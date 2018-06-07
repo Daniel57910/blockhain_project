2495673adba681e9d35f3f507af5566e1c14dbf4
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-07 11:29:37 +0100

    feature test sign in patient from homepage

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -24,3 +24,21 @@ describe('My fourth Test', function() {
     cy.url().should('include', '/doctor/sign_in')
   });
 });
+
+describe('My fifth Test', function() {
+  it("clicking 'Register' navigates to a new url", function() {
+    cy.visit('localhost:9000')
+
+    cy.contains('Register').click()
+    cy.url().should('include', '/doctor/new_registration')
+  });
+});
+
+describe('My fourth Test', function() {
+  it("clicking 'Sign In' navigates to a new url", function() {
+    cy.visit('localhost:9000')
+
+    cy.get('#patient_sign_in').contains('Sign In').click()
+    cy.url().should('include', '/patient/sign_in')
+  });
+});
