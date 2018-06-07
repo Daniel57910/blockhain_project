acf76c1a9ba48647c14444e022d43f98cfc6bc1d
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-07 11:13:58 +0100

    4 feature tests for homepage and sign_in

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,5 +1,26 @@
 describe('My First Test', function() {
   it('Does not do much!', function() {
     expect(true).to.equal(true)
-  })
-})
+  });
+});
+
+describe('My second Test', function() {
+  it('Visits homepage', function() {
+    cy.visit('localhost:9000')
+  });
+});
+
+describe('My third Test', function() {
+  it('finds the content "Medical Prescription Blockchain Application "', function() {
+    cy.visit('localhost:9000')
+  });
+});
+
+describe('My fourth Test', function() {
+  it("clicking 'Sign In' navigates to a new url", function() {
+    cy.visit('localhost:9000')
+
+    cy.contains('Sign In').click()
+    cy.url().should('include', '/doctor/sign_in')
+  });
+});
